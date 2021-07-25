5d9794c67a5b7753c5372561ec0caee86e4ac311
zhaoyun1996 <kiet14020248uet@gmail.com>
2021-07-25 16:58:43 +0700

    day code

---
+++ present.js
@@ -0,0 +1,45 @@
+
+var to = 'Duyên!';
+var gift_url = 'https://www.facebook.com/duyen.cao.5030';
+var gift_image_url = 'Duyen1.jpg';
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
